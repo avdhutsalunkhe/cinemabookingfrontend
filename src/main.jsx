94d8060144dd59e4aa7bfec3df9d8416d4d6c77a
Avdhut Salunkhe <150895852+avdhutsalunkhe@users.noreply.github.com>
2025-11-12 22:49:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import { BrowserRouter as Router } from 'react-router-dom'
 // import { ClerkProvider } from '@clerk/clerk-react'
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
-}
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error('Missing Publishable Key')
+// }
 createRoot(document.getElementById('root')).render(
    
   // <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
